5afeca5636775ad81b27980c98e74d27603f3008
LucaVor <lbvoros@gmail.com>
2025-02-04 16:48:17 -0500

    made library

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from .client import MantisClient, SpacePrivacy, DataType, ReducerModels
+from .render_args import RenderArgs
+
+__version__ = "0.1.0"
+__all__ = ['MantisClient', 'SpacePrivacy', 'DataType', 'ReducerModels', 'RenderArgs']
