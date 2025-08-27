b783d80a3f446d623da302fe8f4186278ddb755c
LucaVor <68753597+LucaVor@users.noreply.github.com>
2025-08-27 12:00:50 -0400

    Enhance browser launch options for Colab

--- mantis_sdk/space.py
+++ mantis_sdk/space.py
@@ -31,7 +31,22 @@ class Space:
         
         browser_args = ["--start-maximized"]
         if colab:
-            browser_args.extend(["--no-sandbox", "--disable-setuid-sandbox"])
+            browser_args.extend([
+                "--no-sandbox",
+                "--disable-setuid-sandbox",
+                "--disable-dev-shm-usage",   # <= critical on Colab
+                "--single-process",          # collapse processes (fewer context switches)
+                "--no-zygote",
+                "--disable-gpu",
+                "--disable-features=site-per-process",
+                "--disable-background-networking",
+                "--disable-background-timer-throttling",
+                "--disable-renderer-backgrounding",
+                "--mute-audio",
+                "--no-first-run",
+                "--no-default-browser-check",
+                "--hide-scrollbars",
+            ])
         
         instance.browser = await instance.playwright.chromium.launch(headless=instance.headless, args=browser_args)
         await instance._init_space()
