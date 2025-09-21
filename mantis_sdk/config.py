2f75b0ac891087d7dfd708c286a09a3da77fab1b
LucaVor <68753597+LucaVor@users.noreply.github.com>
2025-09-21 14:24:24 -0400

    Add backend_host configuration to ConfigurationManager

--- mantis_sdk/config.py
+++ mantis_sdk/config.py
@@ -5,6 +5,7 @@ class ConfigurationManager:
     def __init__(self):
         # Default values
         self.host = os.getenv("MANTIS_HOST", "http://localhost:3000")
+        self.backend_host = os.getenv("MANTIS_BACKEND_HOST", "http://localhost:8000")
         self.domain = os.getenv("MANTIS_DOMAIN", "localhost")
         self.timeout = int(os.getenv("MANTIS_TIMEOUT", "60000"))
         self.render_args = RenderArgs({
